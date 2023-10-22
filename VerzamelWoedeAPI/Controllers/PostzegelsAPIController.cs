d2eb5ee41da36db67094c3bbb0380e02e049dc8d
Nan Ferdinandus <2105792ferdinandus@zuyd.nl>
2023-10-22 17:52:17 +0200

    UpdatedAPI (Model:Postzegels)

---
+++ VerzamelWoedeAPI/Controllers/PostzegelsAPIController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp_TestApp_W3_InlvrOpdr.Models;
+
+namespace VerzamelWoedeAPI.Controllers
+{
+    [Route("api/postzegels")]
+    [ApiController]
+    public class PostzegelsAPIController : ControllerBase
+    {
+        private readonly AppDBContext _context;
+
+        public PostzegelsAPIController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/postzegels
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Postzegel>>> GetPostzegels()
+        {
+            var postzegels = await _context.Postzegels.Include(p => p.Eigenaar).ToListAsync();
+            return postzegels;
+        }
+
+        // GET: api/postzegels/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Postzegel>> GetPostzegel(int id)
+        {
+            var postzegel = await _context.Postzegels.Include(p => p.Eigenaar).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (postzegel == null)
+            {
+                return NotFound();
+            }
+
+            return postzegel;
+        }
+
+        // POST: api/postzegels
+        [HttpPost]
+        public async Task<IActionResult> PostPostzegel(Postzegel postzegel)
+        {
+            _context.Postzegels.Add(postzegel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPostzegel", new { id = postzegel.Id }, postzegel);
+        }
+
+        // PUT: api/postzegels/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPostzegel(int id, Postzegel postzegel)
+        {
+            if (id != postzegel.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(postzegel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PostzegelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/postzegels/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePostzegel(int id)
+        {
+            var postzegel = await _context.Postzegels.FindAsync(id);
+            if (postzegel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Postzegels.Remove(postzegel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PostzegelExists(int id)
+        {
+            return (_context.Postzegels?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- VerzamelWoedeAPI/Program.cs
+++ VerzamelWoedeAPI/Program.cs
@@ -1,25 +1,37 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Configure Swagger/OpenAPI
+if (builder.Environment.IsDevelopment())
+{
+    // Register the Swagger generator, defining one or more Swagger documents
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "VerzamelWoedeAPI", Version = "v1" });
+    });
+}
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // Enable Swagger and Swagger UI
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "VerzamelWoedeAPI V1");
+    });
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
+// Map your controllers, including the custom PostzegelsAPIController
 app.MapControllers();
 
 app.Run();
